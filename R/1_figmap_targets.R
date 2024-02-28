c28a0830aa727a4e84cc0d6d28f367fc999d0170
mobro-git <morgansbrowning@gmail.com>
2024-02-28 15:24:59 -0500

    cleanup

--- R/1_figmap_targets.R
+++
@@ -1,7 +0,0 @@
-
-# creates figmap csv paths based on mapping csv
-figmap_csv_path <- function(fig_subject, fig_type) {
-
-  paste0("plot_mapping/", fig_subject, "_", fig_type, ".csv")
-
-}
--- R/2f_make_kaya.R
+++
@@ -1,9 +0,0 @@
-
-#' make_kaya
-#'
-
-make_kaya <- function(clean_data) {
-
-clean_data
-
-}
