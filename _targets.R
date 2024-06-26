f7b6c8ff4fad23d4c334bfa12086a03f90d13c47
Matthew Zwerling <zwerling.matthew@epa.gov>
2024-06-26 14:01:01 -0400

    Kaya figmap

--- _targets.R
+++ _targets.R
@@ -228,7 +228,8 @@ tar_plan(
   nrgco2_db = create_graph("nrgco2", "diff_bar", config, settings, data_long_clean, figmap_nrgco2_diffbar, pngGraphs = TRUE),
   nrgco2_ts = create_graph("nrgco2", "time_series", config, settings, data_long_clean, figmap_nrgco2_timeseries, pngGraphs = TRUE),
   nrgco2_cu = create_graph("nrgco2", "cone_uncertainty", config, settings, data_long_clean, figmap_nrgco2_cone, pngGraphs = TRUE),
-  
+  kaya_ts = create_graph("Kaya", "time_series", config, settings, data_long_index, figmap_kaya_timeseries, pngGraphs = TRUE),
+    
   # markdowns
   tar_render(btr_tables_figs,
              "docs/report/btr_tables_figs.Rmd",
